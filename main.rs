28ea174980f4ed05fce832197e59ee699480f17f
Mitchell Schrock <mitchell.schrock@jpl.nasa.gov>
2024-11-20 17:06:41 -0800

    separate slint file

--- main.rs
+++ main.rs
@@ -1,12 +1,4 @@
-slint::slint!{
-	export component HelloWorld {
-		Text {
-			text: "hello world";
-			color: green;
-		}
-	}
-}
-
+slint::include_modules!();
 fn main() {
     HelloWorld::new().unwrap().run().unwrap();
 }
